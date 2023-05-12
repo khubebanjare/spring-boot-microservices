91cf94f7d64936256e7dd53d0e769d108e53a6b8
khubebanjare <info.khube@gmail.com>
2023-05-12 11:24:18 +0530

    spring security service old project has created.

---
+++ src/main/java/com/khube/spring/security/main/SpringSecurityServiceOldApplication.java
@@ -0,0 +1,13 @@
+package com.khube.spring.security.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityServiceOldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityServiceOldApplication.class, args);
+	}
+
+}
