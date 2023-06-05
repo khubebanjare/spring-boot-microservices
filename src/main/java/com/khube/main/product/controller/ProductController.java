785f2d894d975cae8a13df3d3f9d9f8aea0a1f14
khubebanjare <info.khube@gmail.com>
2023-06-06 01:08:38 +0530

    new changes are added in product-service.

--- src/main/java/com/khube/main/product/controller/ProductController.java
+++ src/main/java/com/khube/main/product/controller/ProductController.java
@@ -4,6 +4,9 @@ import com.khube.main.product.entity.Product;
 import com.khube.main.product.request.ProductRequest;
 import com.khube.main.product.response.ProductResponse;
 import com.khube.main.product.service.ProductService;
+import com.khube.main.product.util.Mapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,23 +21,28 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+    private static final Logger LOGGER = LogManager.getLogger(ProductController.class);
 
     @PostMapping(value = "/products")
     public ResponseEntity<ProductRequest> saveProduct(@RequestBody Product product){
-        System.out.println(product);
+        LOGGER.info("ProductServiceImpl:saveProduct request from client {} " + Mapper.mapToJsonString(product));
         ProductRequest productRequest = productService.saveProduct(product);
+        LOGGER.info("ProductServiceImpl:saveProduct response from service {} " + Mapper.mapToJsonString(productRequest));
         return new ResponseEntity<ProductRequest>(productRequest, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/products")
     public ResponseEntity<List<ProductResponse>> getProducts(){
         List<ProductResponse> productResponses = productService.getProducts();
+        LOGGER.info("ProductServiceImpl:getProducts response from service {} " + productResponses);
         return new ResponseEntity<>(productResponses, HttpStatus.OK);
     }
 
     @GetMapping(value = "/products/{productId}")
     public ResponseEntity<Optional<ProductResponse>> getProductById(@PathVariable Integer productId){
+        LOGGER.info("ProductServiceImpl:getProducts request from client {} " + productId);
         Optional<ProductResponse> optionalProduct = productService.getProductById(productId);
+        LOGGER.info("ProductServiceImpl:getProducts response from service {} " + optionalProduct);
         return new ResponseEntity<Optional<ProductResponse>>(optionalProduct, HttpStatus.OK);
     }
 }
--- src/main/java/com/khube/main/product/service/impl/ProductServiceImpl.java
+++ src/main/java/com/khube/main/product/service/impl/ProductServiceImpl.java
@@ -9,6 +9,9 @@ import com.khube.main.product.repository.ProductRepository;
 import com.khube.main.product.request.ProductRequest;
 import com.khube.main.product.response.ProductResponse;
 import com.khube.main.product.service.ProductService;
+import com.khube.main.product.util.Mapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +22,7 @@ import java.util.Optional;
 @Service
 public class ProductServiceImpl implements ProductService {
 
+    private static final Logger LOGGER = LogManager.getLogger(ProductServiceImpl.class);
     @Autowired
     private ProductRepository productRepository;
     @Autowired
@@ -28,37 +32,52 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductRequest saveProduct(Product product) {
+        LOGGER.info("ProductServiceImpl:saveProduct execution started...");
        if(productRepository.existsById(product.getProductId()))
-    	   throw new ProductAlreadyPresent("Product Already prseent...");
-        
+    	   throw new ProductAlreadyPresent("Product Already present...");
+
+        LOGGER.debug("ProductServiceImpl:saveProduct Request payload {} " + Mapper.mapToJsonString(product));
        Product newProduct = productRepository.save(product);
+        LOGGER.debug("ProductServiceImpl:saveProduct response payload {} " + Mapper.mapToJsonString(product));
        productRequest = ProductHelper.setProductDetailsForRequest(newProduct);
+        LOGGER.debug("ProductServiceImpl:saveProduct Response {} " + Mapper.mapToJsonString(product));
+        LOGGER.info("ProductServiceImpl:saveProduct execution ended....");
        return productRequest;
     }
 
     @Override
     public List<ProductResponse> getProducts() {
+        LOGGER.info("ProductServiceImpl:getProducts execution started....");
         List<Product> products = productRepository.findAll();
+        LOGGER.info("ProductServiceImpl:getProducts getting all products {} " + products);
         List<ProductResponse> productResponses = new ArrayList<ProductResponse>();
+        LOGGER.debug("ProductServiceImpl:getProducts Fetching all product one by one..");
         if(!products.isEmpty()){
             products.forEach(product -> {
                 productResponse = ProductHelper.setProductDetailsForResponse(product);
+                LOGGER.info("ProductServiceImpl:getProducts converting response type {} " + productResponse);
+                LOGGER.info("ProductServiceImpl:getProducts ....");
                 productResponses.add(productResponse);
             });
         }
         else
             throw new ProductNotFoundException("Product Not Found");
+        LOGGER.info("ProductServiceImpl:getProducts execution ended....");
         return productResponses;
     }
 
     // hello new line added on this project
     @Override
     public Optional<ProductResponse> getProductById(Integer productId) {
+        LOGGER.info("ProductServiceImpl:getProductById execution started....");
         Product existProduct = productRepository.findById(productId)
                                 .orElseThrow(
                                 () -> new ProductIsNotPresent("Product is not present for given Product ID!!! : " + productId));
+
         productResponse = ProductHelper.setProductDetailsForResponse(existProduct);
+        LOGGER.info("ProductServiceImpl:getProductById converting response type {} " + productResponse);
         Optional<ProductResponse> productResponseOptional = Optional.of(productResponse);
+        LOGGER.info("ProductServiceImpl:getProductById adding in optional class {} " + productResponse);
         if(productResponseOptional.isPresent())
             return productResponseOptional;
         else
