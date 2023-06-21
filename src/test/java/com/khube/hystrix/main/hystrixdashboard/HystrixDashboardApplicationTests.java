ace8ffe00459eac0ea3e134a5d5b46e01e6da9ee
khubebanjare <info.khube@gmail.com>
2023-06-21 14:03:14 +0530

    Hystrix Dashboard has createdy by Khube Banjare.

---
+++ src/test/java/com/khube/hystrix/main/hystrixdashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khube.hystrix.main.hystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
