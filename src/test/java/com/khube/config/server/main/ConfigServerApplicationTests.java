4b6c9febfc2a82755b1a74401a168b8fefcc28e7
khubebanjare <info.khube@gmail.com>
2023-06-22 13:52:11 +0530

    Config Server new project has been created by Khube Banjare:

---
+++ src/test/java/com/khube/config/server/main/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khube.config.server.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
